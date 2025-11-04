4cd7c3e5663161e2e0ab7a106bbfa00f8462534e
Adam Ruzicka <a.ruzicka@outlook.com>
2025-11-04 23:28:01 +0100

    Pull in compsize

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y git-annex
+dnf5 install -y git-annex compsize
 
 # Use a COPR Example:
 #
