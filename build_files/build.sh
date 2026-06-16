691ea141d9ccc0aa8d287fa936f2135eb377ce57
Adam Ruzicka <a.ruzicka@outlook.com>
2026-06-16 23:15:45 +0200

    Add qemu

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y git-annex compsize wget incus
+dnf5 install -y git-annex compsize wget \
+	incus qemu-system-x86_64
 
 # Use a COPR Example:
 #
