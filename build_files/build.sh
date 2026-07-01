9fa4c3bad964b77cd817b48fa5f4eed9a6177c7a
Adam Ruzicka <a.ruzicka@outlook.com>
2026-07-01 11:38:00 +0000

    Bring back just

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y git-annex compsize wget \
+dnf5 install -y git-annex compsize wget just \
 	incus qemu-system-x86_64 qemu-device-display-virtio-vga
 
 # Use a COPR Example:
